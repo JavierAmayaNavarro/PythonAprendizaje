190dd94329365ae23cbd189e8a40fb143d7da489
Javier Amaya <javieramayanavarro07@gmail.com>
2026-01-15 17:19:57 +0100

    Modificaciones

--- CITED/UT_2/Proyecto_UT2/Simular_manejo_cuenta_bancaria.py
+++ CITED/UT_2/Proyecto_UT2/Simular_manejo_cuenta_bancaria.py
@@ -1,15 +1,3 @@
-# -El alumnado deberá programar un sistema que simule el manejo de una cuenta bancaria sencilla mediante un menú interactivo.
-# El programa permitirá:
-# # -	Consultar el saldo: Mostrará el dinero que posee el usuario
-# -	Ingresar dinero: Se añadirá al saldo
-# -	Retirar dinero: Se quitará del saldo
-# -	Ver el historial resumido de operaciones: Esta opción deberá ser capaz de mostrar estos mensajes.
-# “Has hecho X ingresos por un total de X €”
-# “Has hecho X retiradas por un total de X €”
-# -	Ver historial de operaciones: Las tres últimas operaciones (sin usar listas)
-# -	Salir del programa: Con opción de confirmar salida antes de salir
-
-
 saldo = 0
 totalingresos = 0
 totalretiradas = 0
