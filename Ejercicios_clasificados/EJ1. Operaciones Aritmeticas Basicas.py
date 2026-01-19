05cc6277631d0e30ad2d7830ba4292bb12ce86d6
Javier Amaya <javieramayanavarro07@gmail.com>
2026-01-19 17:46:09 +0100

    Implementacion Ejercicio

--- Ejercicios_clasificados/EJ1. Operaciones Aritmeticas Basicas.py
+++ Ejercicios_clasificados/EJ1. Operaciones Aritmeticas Basicas.py
@@ -14,3 +14,8 @@ suma = a + b
 resta = a - b 
 multiplicacion = a * b 
 division= a/b
+
+print("Suma: ", suma)
+print("Resta" , resta)
+print("Multuplicacion: ", multiplicacion)
+print("Division: ", division)
