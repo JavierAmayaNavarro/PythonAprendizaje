8035d724e1cc2728c64c7f0261dd02ac5a191121
Javier Amaya <javieramayanavarro07@gmail.com>
2026-01-19 17:42:59 +0100

    Implementacion de ejercicios

---
+++ Ejercicios_clasificados/EJ1. Operaciones Aritmeticas Basicas.py
@@ -0,0 +1,16 @@
+# Diseñar una aplicación en Python que declare dos números enteros y calcule:
+# La suma
+# La resta
+# La multiplicación
+# La división
+# El programa deberá mostrar los resultados por pantalla.
+
+a = 5
+b = 2
+
+
+
+suma = a + b 
+resta = a - b 
+multiplicacion = a * b 
+division= a/b
